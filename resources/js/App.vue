9801da3303b10188dffa065ca849d25873ff1cb7
Showmik4 <showmik.aiub@gmail.com>
2025-06-27 00:15:05 -0700

    Vue Js Setup

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <h1>Hello Vue Lover</h1>
+        <router-link :to="{name:'AddNew'}">Add New</router-link>
+        <router-view />
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App); 
+app.use(router);            // use router on the app instance
+app.mount('#content');      // mount the app
